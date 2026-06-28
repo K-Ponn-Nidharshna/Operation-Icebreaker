bad2b5afb0b0d25b80d1df5e0dc17b77adca8013
K-Ponn-Nidharshna <nidharshna111@gmail.com>
2026-06-28 16:31:01 +0530

    Day 3: Ice Integrity - File Integrity Checker added

--- day3-ice-integrity/file_integrity_checker.py
+++ day3-ice-integrity/file_integrity_checker.py
@@ -8,7 +8,7 @@ def compute_hash(filepath):
     sha256 = hashlib.sha256()
     with open(filepath, "rb") as f:
         while chunk := f.read(8192):
-            sha256.update(chunk)
+            sha256.update(chunk) 
     return sha256.hexdigest()
 
 def save_hash(filepath):
